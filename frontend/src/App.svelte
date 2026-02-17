3f3d33fbdd0e01f649f313621af55f3c08798ebb
Yiyoung Liu <yiyoung.md@gmail.com>
2026-02-16 22:41:29 -0500

    feat: made into two columns, for each individual bus stop

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,8 +3,24 @@
 </script>
 
 <main>
-  <BusTimeEntry route={'61C'} location={'Downtown'} time={2} busNumber={6767}/>
-  <BusTimeEntry route={'28X'} location={'Airporto'} time={11} busNumber={1234}/>
-  <BusTimeEntry route={'61D'} location={'Oakland'} time={13} busNumber={8888}/>
-  <BusTimeEntry route={'61A'} location={'CMU'} time={25} busNumber={5032}/>
+  <div class="container" style="justify-content: start; align-items: flex-start">
+      <div class="stack left">
+      <BusTimeEntry route={'61C'} location={'Downtown'} time={2} busNumber={6767}/>
+      <BusTimeEntry route={'28X'} location={'Airporto'} time={11} busNumber={1234}/>
+      <BusTimeEntry route={'61D'} location={'Oakland'} time={13} busNumber={8888}/>
+      <BusTimeEntry route={'61A'} location={'CMU'} time={25} busNumber={5032}/>
+    </div>
+      <div class="stack right">
+      <BusTimeEntry route={'28X'} location={'Airporto'} time={11} busNumber={1234}/>
+      <BusTimeEntry route={'61D'} location={'Oakland'} time={13} busNumber={8888}/>
+      <BusTimeEntry route={'61C'} location={'Downtown'} time={2} busNumber={6767}/>
+    </div>
+  </div>
 </main>
+
+<style>
+  .stack {
+    flex: 1 1 0;
+    gap: 0.75rem;
+  }
+</style>
--- frontend/src/lib/BusTimeEntry.svelte
+++ frontend/src/lib/BusTimeEntry.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="rounded-box container">
-  <div class="left-stack left">
+  <div class="stack left">
     <div class="route">
       {route}
     </div>
@@ -14,7 +14,7 @@
       To {location.toUpperCase()}
     </div>
   </div>
-  <div class="right-stack right">
+  <div class="stack right">
     <div class="time">
       {time} MIN
     </div>
@@ -30,36 +30,9 @@
     padding: 20px;
     background-color: #f0f0f0;
     border: 1px solid #ccc;
-    min-width: 100vw;
+    min-width: 100%;
     min-height: 80px;
     box-sizing: border-box;
-  }
-
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    width: 100%;
-    box-sizing: border-box;
-    padding: 1rem;
-    border: 1px solid #ccc;
-  }
-
-  .left, .right {
-    padding: 10px;
-  }
-
-  .left-stack {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
-
-  .right-stack {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
   }
 
   .route {
