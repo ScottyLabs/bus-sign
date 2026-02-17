041027724aa4b74b63255348e0a5be1ff91de0f6
Yiyoung Liu <yiyoung.md@gmail.com>
2026-02-16 21:56:56 -0500

    feat: basic bus time entry

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+  import BusTimeEntry from './lib/BusTimeEntry.svelte';
+</script>
+
 <main>
+  <BusTimeEntry />
+  <BusTimeEntry />
+  <BusTimeEntry />
+  <BusTimeEntry />
 </main>
---
+++ frontend/src/lib/BusTimeEntry.svelte
@@ -0,0 +1,73 @@
+<div class="rounded-box container">
+  <div class="left-stack left">
+    <div class="route">
+      61C
+    </div>
+    <div class="location">
+      To Somewhere
+    </div>
+  </div>
+  <div class="right-stack right">
+    <div class="time">
+      2 MIN
+    </div>
+    <div class="bus-number">
+      6767
+    </div>
+  </div>
+</div>
+
+<style>
+  .rounded-box {
+    border-radius: 16px;
+    padding: 20px;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    min-width: 100vw;
+    min-height: 80px;
+    box-sizing: border-box;
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    box-sizing: border-box;
+    padding: 1rem;
+    border: 1px solid #ccc;
+  }
+
+  .left, .right {
+    padding: 10px;
+  }
+
+  .left-stack {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .right-stack {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+  .route {
+    font-size: 40px;
+  }
+
+  .time {
+    font-size: 32px;
+  }
+
+  .location {
+
+  }
+
+  .bus-number {
+
+  }
+</style>
