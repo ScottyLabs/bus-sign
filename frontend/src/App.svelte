a0b0df67ae4eb31eff694f3de608788a5b7a4ca3
Yiyoung Liu <yiyoung.md@gmail.com>
2026-02-16 23:36:24 -0500

    feat: made time entries dynamic over an array

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
   import BusTimeEntry from './lib/BusTimeEntry.svelte';
   import QRcode from './assets/QRcode.png';
+
+  let entriesUC = [
+    { route: '61C', location: 'Downtown', time: 2, busNumber: 6767 },
+    { route: '28X', location: 'Airporto', time: 11, busNumber: 1234 },
+    { route: '61D', location: 'Oakland', time: 13, busNumber: 8888 },
+    { route: '61A', location: 'CMU', time: 25, busNumber: 5032 },
+  ];
+
+  let entriesTep = [
+    { route: '28X', location: 'Airporto', time: 11, busNumber: 1234 },
+    { route: '61D', location: 'Oakland', time: 13, busNumber: 8888 },
+    { route: '61C', location: 'Downtown', time: 67, busNumber: 6767 },
+  ];
 </script>
 
 <main>
@@ -9,18 +22,21 @@
       <div style="font-size: 40px">
         UC Side (Stop 7117)
       </div>
-      <BusTimeEntry route={'61C'} location={'Downtown'} time={2} busNumber={6767}/>
-      <BusTimeEntry route={'28X'} location={'Airporto'} time={11} busNumber={1234}/>
-      <BusTimeEntry route={'61D'} location={'Oakland'} time={13} busNumber={8888}/>
-      <BusTimeEntry route={'61A'} location={'CMU'} time={25} busNumber={5032}/>
+      {#each entriesUC as entry (entry.busNumber)}
+        <BusTimeEntry {...entry} />
+      {:else}
+        <BusTimeEntry route={'No Buses Running'} location={''} time={''} busNumber={''}/>
+      {/each}
     </div>
     <div class="stack left">
       <div style="font-size: 40px">
         Tepper Side (Stop 4407)
       </div>
-      <BusTimeEntry route={'28X'} location={'Airporto'} time={11} busNumber={1234}/>
-      <BusTimeEntry route={'61D'} location={'Oakland'} time={13} busNumber={8888}/>
-      <BusTimeEntry route={'61C'} location={'Downtown'} time={2} busNumber={6767}/>
+      {#each entriesTep as entry (entry.busNumber)}
+        <BusTimeEntry {...entry} />
+      {:else}
+        <BusTimeEntry route={'No Buses Running'} location={''} time={''} busNumber={''}/>
+      {/each}
     </div>
   </div>
   <div class="footer">
