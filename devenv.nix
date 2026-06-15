66f710bda47eae8754f0c4ad990716ef8904ade0
thesuperRL <ryanli1366@gmail.com>
2026-06-15 16:53:01 -0400

    backend: auth: secretspec integration fix and PRT api key

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,33 @@
 { pkgs, config, inputs, ... }:
 
 let
+  b2n = inputs.bun2nix.packages.${pkgs.stdenv.system}.default;
+
+  frontend = b2n.mkDerivation {
+    pname = "bus-sign-frontend";
+    version = (builtins.fromJSON (builtins.readFile ./frontend/package.json)).version;
+    src = ./frontend;
+
+    bunDeps = b2n.fetchBunDeps {
+      bunNix = ./frontend/bun.nix;
+    };
+
+    buildPhase = ''
+      bun run build
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r dist/* $out/
+    '';
+  };
+
   cargoNix = pkgs.callPackage ./Cargo.nix { };
-  backend = cargoNix.rootCrate.build;
+  backend = cargoNix.rootCrate.build.overrideAttrs (old: {
+    postInstall = (old.postInstall or "") + ''
+      cp -r ${frontend} $out/static
+    '';
+  });
 in
 {
   imports = [ inputs.scottylabs.devenvModules.default ];
@@ -18,9 +43,15 @@ in
     };
   };
 
+  cachix.enable = false;
+
   packages = [
     inputs.bun2nix.packages.${pkgs.stdenv.system}.default
   ];
 
   outputs = { inherit backend; };
+
+  processes.backend.exec = "secretspec run --profile dev -- cargo run -p scottylabs_bus_backend";
+
+  env.VAULT_ADDR = "https://secrets2.scottylabs.org";
 }
