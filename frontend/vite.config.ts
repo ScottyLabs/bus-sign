148be60c8878944a1914bce21e38d0161374879f
Yiyoung Liu <yiyoungl@andrew.cmu.edu>
2026-07-31 22:53:26 -0400

    feat(frontend): add tailwind
    Signed-off-by: Yiyoung Liu <yiyoungl@andrew.cmu.edu>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   envDir: "../",
-  plugins: [svelte()],
+  plugins: [tailwindcss(), svelte()],
 });
