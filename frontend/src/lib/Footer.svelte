b1b6ea284b2e4e000ab448e25ea1b6582e4e30bf
Yiyoung Liu <yiyoungl@andrew.cmu.edu>
2026-08-10 09:52:54 -0400

    feat(frontend): dynamic footer
    Signed-off-by: Yiyoung Liu <yiyoungl@andrew.cmu.edu>

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
     import RefreshCW from "~icons/lucide/refresh-cw";
+
+    export let lastUpdated: string = "";
 </script>
 
-<div class="flex shrink-0 justify-between items-center py-3 px-10 shadow-[0_-1px_4px_0px_rgba(0,0,0,0.13)]">
+<div
+    class="flex shrink-0 justify-between items-center py-3 px-10 shadow-[0_-1px_4px_0px_rgba(0,0,0,0.13)]"
+>
     <div class="flex flex-row gap-2 items-center">
         <RefreshCW class="size-4" />
-        <div>
-            Last updated: 5:29 PM
-        </div>
+        <div>Last updated: {lastUpdated}</div>
     </div>
     <div class="flex flex-row gap-2 items-center">
-        <img class="h-[22px] w-[25px]" src="/ScottyLabs.svg" alt="ScottyLabs Logo" />
+        <img
+            class="h-[22px] w-[25px]"
+            src="/ScottyLabs.svg"
+            alt="ScottyLabs Logo"
+        />
         <div>
-            Made by ScottyLabs in collaboration with the Undergraduate Student Senate
+            Made by ScottyLabs in collaboration with the Undergraduate Student
+            Senate
         </div>
     </div>
 </div>
